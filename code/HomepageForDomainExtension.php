cdd04a9b8d749ba9d7497790fed3219f5fbd26d2
helpfulrobot <chris@silverstripe.com>
2015-12-20 17:36:33 +1300

    Converted to PSR-2

--- code/HomepageForDomainExtension.php
+++ code/HomepageForDomainExtension.php
@@ -1,62 +1,77 @@
 <?php
-class HomepageForDomainExtension extends DataExtension {
+class HomepageForDomainExtension extends DataExtension
+{
 
-	/**
-	 * Whether or not to write the homepage map for static publisher
-	 */
-	public static $write_homepage_map = true;
+    /**
+     * Whether or not to write the homepage map for static publisher
+     */
+    public static $write_homepage_map = true;
 
-	private static $db = array(
-		"HomepageForDomain" => "Varchar(100)",
-	);
+    private static $db = array(
+        "HomepageForDomain" => "Varchar(100)",
+    );
 
-	public function updateSettingsFields(&$fields) {
-		$fields->addFieldsToTab("Root.Settings", array(
-			new LiteralField(
-				"HomepageForDomainInfo", 
-				"<p>" . 
-					_t('SiteTree.NOTEUSEASHOMEPAGE', 
-					"Use this page as the 'home page' for the following domains: 
+    public function updateSettingsFields(&$fields)
+    {
+        $fields->addFieldsToTab("Root.Settings", array(
+            new LiteralField(
+                "HomepageForDomainInfo",
+                "<p>" .
+                    _t('SiteTree.NOTEUSEASHOMEPAGE',
+                    "Use this page as the 'home page' for the following domains: 
 					(separate multiple domains with commas)") .
-				"</p>"
-			),
-			new TextField(
-				"HomepageForDomain",
-				_t('SiteTree.HOMEPAGEFORDOMAIN', "Domain(s)", 'Listing domains that should be used as homepage')
-			)
-		));
-	}
+                "</p>"
+            ),
+            new TextField(
+                "HomepageForDomain",
+                _t('SiteTree.HOMEPAGEFORDOMAIN', "Domain(s)", 'Listing domains that should be used as homepage')
+            )
+        ));
+    }
 
-	public function onAfterPublish() {
-		// Check to write CMS homepage map.
-		$usingStaticPublishing = false;
-		foreach(ClassInfo::subclassesFor('StaticPublisher') as $class) {
-			if ($this->owner->hasExtension($class)) $usingStaticPublishing = true;
-		}
+    public function onAfterPublish()
+    {
+        // Check to write CMS homepage map.
+        $usingStaticPublishing = false;
+        foreach (ClassInfo::subclassesFor('StaticPublisher') as $class) {
+            if ($this->owner->hasExtension($class)) {
+                $usingStaticPublishing = true;
+            }
+        }
 
-		// Ff you change the path here, you must also change it in sapphire/static-main.php
-		if (self::$write_homepage_map) {
-			if ($usingStaticPublishing && $map = self::generate_homepage_domain_map()) {
-				@file_put_contents(BASE_PATH.'/'.ASSETS_DIR.'/_homepage-map.php', "<?php\n\$homepageMap = ".var_export($map, true)."; ?>");
-			} else { if (file_exists(BASE_PATH.'/'.ASSETS_DIR.'/_homepage-map.php')) unlink(BASE_PATH.'/'.ASSETS_DIR.'/_homepage-map.php'); }
-		}
-	}
+        // Ff you change the path here, you must also change it in sapphire/static-main.php
+        if (self::$write_homepage_map) {
+            if ($usingStaticPublishing && $map = self::generate_homepage_domain_map()) {
+                @file_put_contents(BASE_PATH.'/'.ASSETS_DIR.'/_homepage-map.php', "<?php\n\$homepageMap = ".var_export($map, true)."; ?>");
+            } else {
+                if (file_exists(BASE_PATH.'/'.ASSETS_DIR.'/_homepage-map.php')) {
+                    unlink(BASE_PATH.'/'.ASSETS_DIR.'/_homepage-map.php');
+                }
+            }
+        }
+    }
 
-	public function updateFieldLabels(&$labels) {
-		$labels['HomepageForDomain'] = _t('SiteTree.HomepageForDomain', 'Hompage for this domain');
-	}
+    public function updateFieldLabels(&$labels)
+    {
+        $labels['HomepageForDomain'] = _t('SiteTree.HomepageForDomain', 'Hompage for this domain');
+    }
 
-	/**
-	 * @return Array
-	 */
-	public static function generate_homepage_domain_map() {
-		$domainSpecificHomepages = Versioned::get_by_stage('Page', 'Live', "\"HomepageForDomain\" != ''", "\"URLSegment\" ASC");
-		if (!$domainSpecificHomepages) return false;
-		
-		$map = array();
-		foreach($domainSpecificHomepages->map('URLSegment', 'HomepageForDomain') as $url => $domains) {
-			foreach(explode(',', $domains) as $domain) $map[$domain] = $url;
-		}
-		return $map;
-	}
+    /**
+     * @return Array
+     */
+    public static function generate_homepage_domain_map()
+    {
+        $domainSpecificHomepages = Versioned::get_by_stage('Page', 'Live', "\"HomepageForDomain\" != ''", "\"URLSegment\" ASC");
+        if (!$domainSpecificHomepages) {
+            return false;
+        }
+        
+        $map = array();
+        foreach ($domainSpecificHomepages->map('URLSegment', 'HomepageForDomain') as $url => $domains) {
+            foreach (explode(',', $domains) as $domain) {
+                $map[$domain] = $url;
+            }
+        }
+        return $map;
+    }
 }
--- tests/HomepageForDomainTest.php
+++ tests/HomepageForDomainTest.php
@@ -1,130 +1,135 @@
 <?php
-class HomepageForDomainTest extends SapphireTest {
-	
-	protected $usesDatabase = true;
+class HomepageForDomainTest extends SapphireTest
+{
+    
+    protected $usesDatabase = true;
 
-	static $fixture_file = 'HomepageForDomainTest.yml';
+    public static $fixture_file = 'HomepageForDomainTest.yml';
 
-	function setUp() {
-		parent::setUp();
-		
-		Object::add_extension("SiteTree", "FilesystemPublisher('assets/HomepageForDomainTest-static-folder/')");
-		HomepageForDomainExtension::$write_homepage_map = false;
-		
-		$this->orig['domain_based_caching'] = FilesystemPublisher::$domain_based_caching;
-		FilesystemPublisher::$domain_based_caching = false;
-	}
-	
-	function tearDown() {
-		parent::tearDown();
+    public function setUp()
+    {
+        parent::setUp();
+        
+        Object::add_extension("SiteTree", "FilesystemPublisher('assets/HomepageForDomainTest-static-folder/')");
+        HomepageForDomainExtension::$write_homepage_map = false;
+        
+        $this->orig['domain_based_caching'] = FilesystemPublisher::$domain_based_caching;
+        FilesystemPublisher::$domain_based_caching = false;
+    }
+    
+    public function tearDown()
+    {
+        parent::tearDown();
 
-		Object::remove_extension("SiteTree", "FilesystemPublisher('assets/HomepageForDomainTest-static-folder/')");
-		HomepageForDomainExtension::$write_homepage_map = true;
+        Object::remove_extension("SiteTree", "FilesystemPublisher('assets/HomepageForDomainTest-static-folder/')");
+        HomepageForDomainExtension::$write_homepage_map = true;
 
-		FilesystemPublisher::$domain_based_caching = $this->orig['domain_based_caching'];
+        FilesystemPublisher::$domain_based_caching = $this->orig['domain_based_caching'];
 
-		if(file_exists(BASE_PATH . '/assets/HomepageForDomainTest-static-folder')) {
-			Filesystem::removeFolder(BASE_PATH . '/assets/HomepageForDomainTest-static-folder');
-		}
-	}
-	
-	function testStaticPublishing() {
-		$this->logInWithPermission('ADMIN');
-		
-		$p1 = new Page();
-		$p1->URLSegment = strtolower(__CLASS__).'-page-1';
-		$p1->HomepageForDomain = '';
-		$p1->write();
-		$p1->doPublish();
-		$p2 = new Page();
-		$p2->URLSegment = strtolower(__CLASS__).'-page-2';
-		$p2->HomepageForDomain = 'domain1';
-		$p2->write();
-		$p2->doPublish();
-		$p3 = new Page();
-		$p3->URLSegment = strtolower(__CLASS__).'-page-3';
-		$p3->HomepageForDomain = 'domain2,domain3';
-		$p3->write();
-		$p3->doPublish();
-		
-		$map = HomepageForDomainExtension::generate_homepage_domain_map();
-		
-		$this->assertEquals(
-			$map, 
-			array(
-				'domain1' => strtolower(__CLASS__).'-page-2',
-				'domain2' => strtolower(__CLASS__).'-page-3',
-				'domain3' => strtolower(__CLASS__).'-page-3',
-			), 
-			'Homepage/domain map is correct when static publishing is enabled'
-		);
-	}
+        if (file_exists(BASE_PATH . '/assets/HomepageForDomainTest-static-folder')) {
+            Filesystem::removeFolder(BASE_PATH . '/assets/HomepageForDomainTest-static-folder');
+        }
+    }
+    
+    public function testStaticPublishing()
+    {
+        $this->logInWithPermission('ADMIN');
+        
+        $p1 = new Page();
+        $p1->URLSegment = strtolower(__CLASS__).'-page-1';
+        $p1->HomepageForDomain = '';
+        $p1->write();
+        $p1->doPublish();
+        $p2 = new Page();
+        $p2->URLSegment = strtolower(__CLASS__).'-page-2';
+        $p2->HomepageForDomain = 'domain1';
+        $p2->write();
+        $p2->doPublish();
+        $p3 = new Page();
+        $p3->URLSegment = strtolower(__CLASS__).'-page-3';
+        $p3->HomepageForDomain = 'domain2,domain3';
+        $p3->write();
+        $p3->doPublish();
+        
+        $map = HomepageForDomainExtension::generate_homepage_domain_map();
+        
+        $this->assertEquals(
+            $map,
+            array(
+                'domain1' => strtolower(__CLASS__).'-page-2',
+                'domain2' => strtolower(__CLASS__).'-page-3',
+                'domain3' => strtolower(__CLASS__).'-page-3',
+            ),
+            'Homepage/domain map is correct when static publishing is enabled'
+        );
+    }
 
-	function testRouting() {
-		$originalHost = $_SERVER['HTTP_HOST'];
+    public function testRouting()
+    {
+        $originalHost = $_SERVER['HTTP_HOST'];
 
-		// Tests matching an HTTP_HOST value to URLSegment homepage values
-		$tests = array(
-			'page.co.nz' => 'page1',
-			'www.page.co.nz' => 'page1',
-			'help.com' => 'page1',
-			'www.help.com' => 'page1',
-			'something.com' => 'page1',
-			'www.something.com' => 'page1',
+        // Tests matching an HTTP_HOST value to URLSegment homepage values
+        $tests = array(
+            'page.co.nz' => 'page1',
+            'www.page.co.nz' => 'page1',
+            'help.com' => 'page1',
+            'www.help.com' => 'page1',
+            'something.com' => 'page1',
+            'www.something.com' => 'page1',
 
-	 		'other.co.nz' => 'page2',
-	 		'www.other.co.nz' => 'page2',
-			'right' => 'page2',
-			'www. right' => 'page2',
+            'other.co.nz' => 'page2',
+            'www.other.co.nz' => 'page2',
+            'right' => 'page2',
+            'www. right' => 'page2',
 
-			'only.com' => 'page3',
-			'www.only.com' => 'page3',
-			
-			'www.somethingelse.com' => 'home',
-			'somethingelse.com' => 'home',
-			
-			// Test some potential false matches to page2 and page3
-			'alternate.only.com' => 'home',
-			'www.alternate.only.com' => 'home',
-			'alternate.something.com' => 'home',
-		);
-		
-		foreach($tests as $domain => $urlSegment) {
-			RootURLController::reset();
-			$_SERVER['HTTP_HOST'] = $domain;
-			
-			$this->assertEquals(
-				$urlSegment, 
-				RootURLController::get_homepage_link(), 
-				"Testing $domain matches $urlSegment"
-			);
-		}
-		
-		$_SERVER['HTTP_HOST'] = $originalHost;
-	}	
+            'only.com' => 'page3',
+            'www.only.com' => 'page3',
+            
+            'www.somethingelse.com' => 'home',
+            'somethingelse.com' => 'home',
+            
+            // Test some potential false matches to page2 and page3
+            'alternate.only.com' => 'home',
+            'www.alternate.only.com' => 'home',
+            'alternate.something.com' => 'home',
+        );
+        
+        foreach ($tests as $domain => $urlSegment) {
+            RootURLController::reset();
+            $_SERVER['HTTP_HOST'] = $domain;
+            
+            $this->assertEquals(
+                $urlSegment,
+                RootURLController::get_homepage_link(),
+                "Testing $domain matches $urlSegment"
+            );
+        }
+        
+        $_SERVER['HTTP_HOST'] = $originalHost;
+    }
 
-	public function testGetHomepageLink() {
-		$default = $this->objFromFixture('Page', 'home');
-		$nested  = $this->objFromFixture('Page', 'nested');
-		
-		SiteTree::disable_nested_urls();
-		$this->assertEquals('home', RootURLController::get_homepage_link());
-		SiteTree::enable_nested_urls();
-		$this->assertEquals('home', RootURLController::get_homepage_link());
-		
-		$nested->HomepageForDomain = str_replace('www.', null, $_SERVER['HTTP_HOST']);
-		$nested->write();
-		
-		RootURLController::reset();
-		SiteTree::disable_nested_urls();
-		$this->assertEquals('nested-home', RootURLController::get_homepage_link());
-		
-		RootURLController::reset();
-		SiteTree::enable_nested_urls();
-		$this->assertEquals('home/nested-home', RootURLController::get_homepage_link());
-		
-		$nested->HomepageForDomain = null;
-		$nested->write();
-	}
-	
+    public function testGetHomepageLink()
+    {
+        $default = $this->objFromFixture('Page', 'home');
+        $nested  = $this->objFromFixture('Page', 'nested');
+        
+        SiteTree::disable_nested_urls();
+        $this->assertEquals('home', RootURLController::get_homepage_link());
+        SiteTree::enable_nested_urls();
+        $this->assertEquals('home', RootURLController::get_homepage_link());
+        
+        $nested->HomepageForDomain = str_replace('www.', null, $_SERVER['HTTP_HOST']);
+        $nested->write();
+        
+        RootURLController::reset();
+        SiteTree::disable_nested_urls();
+        $this->assertEquals('nested-home', RootURLController::get_homepage_link());
+        
+        RootURLController::reset();
+        SiteTree::enable_nested_urls();
+        $this->assertEquals('home/nested-home', RootURLController::get_homepage_link());
+        
+        $nested->HomepageForDomain = null;
+        $nested->write();
+    }
 }
