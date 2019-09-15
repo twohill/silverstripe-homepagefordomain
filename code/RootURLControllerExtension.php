1b4be81361ae2c8b45dbc46d173b0bba6cbfb074
Al Twohill <al@twohill.nz>
2019-09-15 12:06:16 +1200

    Add RootURLController Extension now that functionality is removed from core

---
+++ code/RootURLControllerExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Twohill\HomepageForDomain;
+
+
+use SilverStripe\Core\Extension;
+use SilverStripe\CMS\Model\SiteTree;
+
+class RootURLControllerExtension extends Extension
+{
+
+    /**
+     * @param $link
+     * @return string
+     */
+    public function updateHomepageLink(&$link)
+    {
+        $host = str_replace('www.', null, $_SERVER['HTTP_HOST']);
+        $candidates = SiteTree::get()->where([
+            '"SiteTree"."HomepageForDomain" LIKE ?' => "%$host%"
+        ]);
+        if ($candidates) {
+            /** @var SiteTree $candidate */
+            foreach ($candidates as $candidate) {
+                if (preg_match('/(,|^) *' . preg_quote($host) . ' *(,|$)/', $candidate->HomepageForDomain)) {
+                    $link = trim($candidate->RelativeLink(true), '/');
+                }
+            }
+        }
+        return $link;
+    }
+}
